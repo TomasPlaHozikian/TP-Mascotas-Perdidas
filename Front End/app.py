8defc5c9a4aaa7ae5d1590b1ec53fd3a2760f62a
Simon <swallace@fi.uba.ar>
2024-06-05 18:48:34 -0300

    Agrego ruta de una vista al app.py del front, y agrego el boton que hace referencia a esa vista.

--- Front End/app.py
+++ Front End/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/busqueda')
+def buscar():
+    return render_template('buscaranimal.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
