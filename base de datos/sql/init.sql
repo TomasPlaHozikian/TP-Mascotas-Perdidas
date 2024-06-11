34bcdd2e070910ec16198f5ca6b0e96981a698d8
Tomas Pla Hozikian <tpla@fi.uba.ar>
2024-06-11 19:34:57 -0300

    Update init.sql

--- base de datos/sql/init.sql
+++ base de datos/sql/init.sql
@@ -10,7 +10,12 @@ CREATE TABLE IF NOT EXISTS animales (
     nombre VARCHAR(50),
     especie VARCHAR(50),
     raza VARCHAR(50),
-    ubicacion VARCHAR(50)
+    provincia VARCHAR(50),
+    municipio VARCHAR(50),
+    localidad VARCHAR(50),
+    calle VARCHAR(50),
+    numero INT,
+    foto VARCHAR(1000)
 );
 
 CREATE TABLE IF NOT EXISTS centros (
@@ -21,8 +26,9 @@ CREATE TABLE IF NOT EXISTS centros (
 INSERT INTO usuarios (nombre, apellido, mail, numero, contrasena) VALUES ('Juan', 'Perez', 'juan@mail.com',123456789, 'hola12345');
 INSERT INTO usuarios (nombre, apellido, mail, numero, contrasena) VALUES ('Maria', 'Gonzalez', 'maria@mail.com',987654321, 'hola54321');
 
-INSERT INTO animales (nombre, especie, raza, ubicacion) VALUES ('Firulais', 'Perro', 'Labrador', 'Calle 123');
-INSERT INTO animales (nombre, especie, raza, ubicacion) VALUES ('Mishi', 'Gato', 'Siames', 'Calle 456');
+INSERT INTO animales (nombre, especie, raza, provincia, municipio, localidad, calle, numero, foto) VALUES ('Firulais', 'Perro', 'Labrador', 'Buenos Aires', 'Lomas de Zamora', 'Banfield', 'Alvear', 800, 'https://cdn.discordapp.com/attachments/601110619646853134/1249859721423228969/labrador.jpg?ex=6668d612&is=66678492&hm=db205e119841e841ae9dddd506b8c5513dbc8492552d34dfc35bcfa165b42908&');
+INSERT INTO animales (nombre, especie, raza, provincia, municipio, localidad, calle, numero, foto) VALUES ('Mishi', 'Gato', 'Siames', 'Buenos Aires', 'Lanus', 'Remedios De Escalda', 'Lugones', 200, 'https://cdn0.soyungato.com/es/razas/0/2/0/gato-siames_20_9_150_square.jpg');
+INSERT INTO animales (nombre, especie, raza, provincia, municipio, localidad, calle, numero, foto) VALUES ('Jorge', 'Gato', 'Calle', 'Buenos Aires', 'Lanus', 'Valentin Alsina', 'Paraguay', 4000, 'https://www.warrenphotographic.co.uk/photography/sqrs/17693.jpg');
 
 INSERT INTO centros (nombre, direccion) VALUES ('Centro de adopcion de animales', 'Calle 789');
 INSERT INTO centros (nombre, direccion) VALUES ('Centro de adopcion de animales 2', 'Calle 1011');
